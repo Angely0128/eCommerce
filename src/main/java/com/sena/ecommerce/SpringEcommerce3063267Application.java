a9ad5d5af5ac017fe1eb309ffaa15d60cf19b35f
AngelySanchez <flechasangely@gmail.com>
2025-08-13 13:57:12 -0500

    v0.1_clasesPaqueteModelo_AngelySanchez_13082025

--- src/main/java/com/sena/ecommerce/SpringEcommerce3063267Application.java
+++ src/main/java/com/sena/ecommerce/SpringEcommerce3063267Application.java
@@ -3,6 +3,7 @@ package com.sena.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringEcommerce3063267Application {
 
