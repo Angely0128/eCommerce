4dad137ab638f6a7cd2c6bdca44964a6a01c399b
AngelySanchez <flechasangely@gmail.com>
2025-08-14 14:48:10 -0500

    v0.4_creacioninterface&clasesService_AngelySanchez_14082025

--- src/main/java/com/sena/ecommerce/service/IProductoService.java
+++ src/main/java/com/sena/ecommerce/service/IProductoService.java
@@ -6,15 +6,15 @@ import java.util.Optional;
 import com.sena.ecommerce.model.Producto;
 
 public interface IProductoService {
-	
+
 	public Producto save(Producto producto);
-	
+
 	public Optional<Producto> get(Integer id);
-	
+
 	public void update(Producto producto);
-	 
+
 	public void delete(Integer id);
-	
+
 	public List<Producto> findAll();
 
 }
