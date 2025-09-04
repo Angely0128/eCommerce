a2b0968be3d6ea81e0282ebe09f34d0b55de3b6e
angelyFlechas <flechasangely@gmail.com>
2025-09-04 15:30:32 -0500

    v0.12_metodosaddCart&deleteProductosCartclaseHomeUserController_AngelySanchez_04092025

--- src/main/java/com/sena/ecommerce/controller/HomeUserController.java
+++ src/main/java/com/sena/ecommerce/controller/HomeUserController.java
@@ -1,5 +1,8 @@
 package com.sena.ecommerce.controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -9,10 +12,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.sena.ecommerce.model.DetalleOrden;
+import com.sena.ecommerce.model.Orden;
 import com.sena.ecommerce.model.Producto;
+import com.sena.ecommerce.service.IDetalleOrdenService;
+import com.sena.ecommerce.service.IOrdenService;
 import com.sena.ecommerce.service.IProductoService;
+import com.sena.ecommerce.service.IUsuarioService;
 
 @Controller
 @RequestMapping("/")
@@ -26,6 +36,20 @@ public class HomeUserController {
 	@Autowired
 	private IProductoService productoService;
 
+	@Autowired
+	private IUsuarioService usuarioService;
+
+	@Autowired
+	private IOrdenService ordenService;
+
+	@Autowired
+	private IDetalleOrdenService detalleOrden;
+
+	// lista de detalles de la orden para guardar en la db
+	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+	// objeto que almacena
+	Orden orden = new Orden();
+
 	@GetMapping("")
 	public String home(Model model) {
 		model.addAttribute("productos", productoService.findAll());
@@ -36,7 +60,7 @@ public class HomeUserController {
 
 	@GetMapping("productohome/{id}")
 	public String productohome(@PathVariable Integer id, Model model) {
-		LOGGER.warn("Id producto enviado como parametro {}" , id );
+		LOGGER.warn("Id producto enviado como parametro {}", id);
 		// variable de la clase producto
 		Producto p = new Producto();
 		Optional<Producto> op = productoService.get(id);
@@ -44,6 +68,64 @@ public class HomeUserController {
 		p = op.get();
 		model.addAttribute("producto", p);
 		return "usuario/productohome";
+	}
+
+	// metodo para enviar del boton del formulario de producto home al carrito de
+	// compras
+	@PostMapping("/cart")
+	public String addCart(@RequestParam Integer id, @RequestParam Double cantidad, Model model) {
+		DetalleOrden detaorden = new DetalleOrden();
+		Producto p = new Producto();
+		// variable de tipo double que siempre que se ingrese ne le metodo se inicializa
+		// en 0 despues de cada compra
+		double sumaTotal = 0;
+		Optional<Producto> op = productoService.get(id);
+		LOGGER.warn("Producto añadido {}", op.get());
+		LOGGER.warn("Cantidad añadida{}", cantidad);
+		p = op.get();
+		detaorden.setCantidad(cantidad);
+		detaorden.setPrecio(p.getPrecio());
+		detaorden.setNombre(p.getNombre());
+		detaorden.setTotal(p.getPrecio() * cantidad);
+		detaorden.setProducto(p);
+		// validacion para evitar duplicados de productos
+		Integer idProducto = p.getId();
+		// funcion lamda stream y funcion anonima con predicado anyMatch
+		boolean insertado = detalles.stream().anyMatch(prod -> prod.getProducto().getId() == idProducto);
+		// si no es verdadero añade el producto a la llista
+		if (!insertado) {
+			detalles.add(detaorden);
+		}
+		// sumA de totales que el usuario añade al carrito
+		// funciones de java 8 lamda >Stream
+		// funcion anonima java8
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+		// pasar variable a la vista
+		orden.setTotal(sumaTotal);
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		return "usuario/carrito";
+	}
+
+	// metodo para quitar productos del carrito de compras
+	@GetMapping("/delete/cart/{id}")
+	public String deleteProductoCart(@PathVariable Integer id, Model model) {
+		// lista de productos
+		List<DetalleOrden> ordenesNuevas = new ArrayList<DetalleOrden>();
+		// quitar un objeto de la lista detalle orden
+		for (DetalleOrden detalleOrden : detalles) {
+			if (detalleOrden.getProducto().getId() != id) {
+				ordenesNuevas.add(detalleOrden);
+			}
+		}
+		// cargar nueva lista con los productos restantes
+		detalles = ordenesNuevas;
+		// recalcular los totales de la lista
+		double sumaTotal = 0;
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		return "usuario/carrito";
 	}
 
 }
